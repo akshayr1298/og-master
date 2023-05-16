bef21e823c6ba50535d2d2f02b0571713b01039f
sinhaKAN-ra <sinhakaran7701@gmail.com>
2023-05-16 14:48:53 +0530

    user-type added in userSignUP

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,9 @@
 //     databaseID : REACT_APP_DATABASE_ID,
 // }
 
-// REACT_APP_AW_END_POINT = 
-
-// REACT_APP_AW_PROJECT_ID = 
-
-// REACT_APP_AW_DATABASE_ID = 
+export const Server = {
+    endpoint : process.env.REACT_APP_ENDPOINT,
+    project: process.env.REACT_APP_PROJECT,
+    collectionID : process.env.REACT_APP_COLLECTION_ID,
+    databaseID : process.env.REACT_APP_DATABASE_ID,
+}
